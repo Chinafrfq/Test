4bce64e0bd0f1aa652353bf24ce7887ed4dac596
ghp <ghp@qq.com>
2022-08-29 16:26:31 +0800

    hot-fix commit2

--- src/main/java/com/hhxy/Test.java
+++ src/main/java/com/hhxy/Test.java
@@ -2,6 +2,9 @@ package com.hhxy;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("这是master分支");
+        System.out.println("这是hot-fix");
+        System.out.println("这是hot-fix");
+        System.out.println("这是hot-fix");
+        System.out.println("这是hot-fix");
     }
 }
