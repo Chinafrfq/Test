cc9faae3b1d61e939ca88d29b0323eb75e4156f8
ghp <ghp@qq.com>
2022-08-29 16:30:48 +0800

    master commit3

--- src/main/java/com/hhxy/Test.java
+++ src/main/java/com/hhxy/Test.java
@@ -2,9 +2,6 @@ package com.hhxy;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("这是hot-fix");
-        System.out.println("这是hot-fix");
-        System.out.println("这是hot-fix");
-        System.out.println("这是hot-fix");
+        System.out.println("这是master");
     }
 }
