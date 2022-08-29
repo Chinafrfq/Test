9f058b026a282574489d6ac86578e57df1d71852
Chinafrfq <105155111+Chinafrfq@users.noreply.github.com>
2022-08-29 17:45:03 +0800

    pull test
    用来测试再IDEA中拉取代码

--- src/main/java/com/hhxy/Test.java
+++ src/main/java/com/hhxy/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("这是master");
         System.out.println("这是ho-fix分支");
         System.out.println("push Test");
+        System.out.println("pull Test");
     }
 }
