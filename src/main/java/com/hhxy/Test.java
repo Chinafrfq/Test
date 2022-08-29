37cd174747e725bcaf9470052f128e4839b1006d
ghp <ghp@qq.com>
2022-08-29 17:15:08 +0800

    push Test commit

--- src/main/java/com/hhxy/Test.java
+++ src/main/java/com/hhxy/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("这是master");
         System.out.println("这是ho-fix分支");
+        System.out.println("push Test");
     }
 }
